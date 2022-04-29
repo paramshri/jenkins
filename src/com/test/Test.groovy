f274696dd02f177d6383e28e5072d4abb4566535
paramshri <31514655+paramshri@users.noreply.github.com>
2022-04-30 02:12:54 +0530

    Add files via upload

--- src/com/test/Test.groovy
+++ src/com/test/Test.groovy
@@ -4,13 +4,8 @@ class Test implements Serializable {
 	Test(jenkins){
 		jenkins.echo "Hi"
 		jenkins.echo jenkins.getClass().toString()
-		Class thisClass = jenkins.getClass;
-            Method[] methods = thisClass.getDeclaredMethods();
-
-            for (int i = 0; i < methods.length; i++) {
-                jenkins.echo methods[i].toString()
-            }
-		jenkins.echo "Hello world"
+		def methods = jenkins.getClass().declaredMethods.findAll { !it.synthetic }.name
+		jenkins.echo $methods
 	}
 
 }
